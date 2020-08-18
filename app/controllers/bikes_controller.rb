4f7acc7a893eb5ea620aa7494d6810b8eed86d2b
Nirali Patel <niralidpatel@outlook.com>
2020-08-18 12:04:56 -0500

    added before-action

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,4 +1,5 @@
 class BikesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
   def index
     @bikes = Bike.all
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
   def destroy 
     @user = User.find(params[:id])
     @user.destroy
