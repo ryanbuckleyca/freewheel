8ed210df21d1f0ba206d465dc4eafa10eea73a3d
deb-anjos <debora_anjos@icloud.com>
2020-08-20 17:15:57 -0400

    geocoding backend completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_181931) do
+ActiveRecord::Schema.define(version: 2020_08_20_192731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 2020_08_18_181931) do
     t.boolean "available_for_rent"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_bikes_on_user_id"
   end
 
