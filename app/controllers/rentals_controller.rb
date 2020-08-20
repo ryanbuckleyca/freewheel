20879ac50e84706249a53756a300b5d2610db816
arthur prats <arthurprats@gmail.com>
2020-08-20 14:50:48 -0400

    see-approved-requests

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -6,4 +6,14 @@ class RentalsController < ApplicationController
   def show
     @rental = Rental.find(@rental)
   end
+
+  # need to add a method update
+  # and then confirmed
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:rental_start, :rental_end, :message, :booking_confirmed)
+  end
+
 end
